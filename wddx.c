52f31f435f075c30eb50556c4cdb2c9d78b2bfe6
Remi Collet <remi@php.net>
2019-05-23 10:43:51 +0200

    display extension version in phpinfo

--- wddx.c
+++ wddx.c
@@ -357,6 +357,7 @@ PHP_MINFO_FUNCTION(wddx)
 	php_info_print_table_start();
 #if HAVE_PHP_SESSION && !defined(COMPILE_DL_SESSION)
 	php_info_print_table_header(2, "WDDX Support", "enabled" );
+	php_info_print_table_header(2, "WDDX extension version", PHP_WDDX_VERSION);
 	php_info_print_table_row(2, "WDDX Session Serializer", "enabled" );
 #else
 	php_info_print_table_row(2, "WDDX Support", "enabled" );
